58e004f22b0da3812a259290b3f6f2b28372ee54
Gleison Lemos <gleison04lemos@gmail.com>
2023-10-18 22:56:19 -0300

    feat: add cache and logic pageLink

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Cache;
 
 class ProjectController extends Controller
 {
@@ -11,23 +12,48 @@ class ProjectController extends Controller
     {
         $username = 'Gleis0nLemos';
         $perPage = 30; 
+        $cacheKey = "github_projects_{$username}";
+        $projects = Cache::get($cacheKey);
 
-        $token = env('GITHUB_TOKEN'); 
-        
-        $url = "https://api.github.com/users/{$username}/repos?per_page={$perPage}";
+        if (!$projects) {
+            $token = env('GITHUB_TOKEN');
 
-        $client = new Client([
-            'headers' => [
-                'Authorization' => "Bearer $token",
-            ],
-        ]);
-
-        $response = $client->get($url);
-
-        if ($response->getStatusCode() === 200) {
-            $projects = json_decode($response->getBody(), true);
-        } else {
+            $page = 1;
             $projects = [];
+
+            do {
+                $url = "https://api.github.com/users/{$username}/repos?page={$page}&per_page={$perPage}";
+
+                $client = new Client([
+                    'headers' => [
+                        'Authorization' => "Bearer $token",
+                    ],
+                ]);
+
+                $response = $client->get($url);
+
+                if ($response->getStatusCode() === 200) {
+                    $data = json_decode($response->getBody(), true);
+                    $projects = array_merge($projects, $data);
+                }
+
+                $nextPageLink = null;
+                $links = $response->getHeader('Link');
+                foreach ($links as $link) {
+                    if (preg_match('/<([^>]+)>; rel="next"/', $link, $matches)) {
+                        $nextPageLink = $matches[1];
+                        break;
+                    }
+                }
+
+                if ($nextPageLink) {
+                    $page++;
+                } else {
+                    break; // Não há mais páginas
+                }
+            } while ($nextPageLink);
+
+            Cache::put($cacheKey, $projects, 6000); // Armazene em cache os dados por 60 minutos
         }
 
         return view('projects', compact('projects'));
